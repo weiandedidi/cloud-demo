d9c4952694027866cdedc4736044e63798b0a112
马启迪 <maqidi@meituan.com>
2018-08-07 11:00:14 +0800

    /HystrixDashboard

---
+++ consumer-hystrix-dashboard/src/main/java/com/qidi/cloud/HystrixDashboardApp.java
@@ -0,0 +1,19 @@
+package com.qidi.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: qidi
+ * Date: 2018/8/7
+ * Time: 上午10:12
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApp {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApp.class, args);
+    }
+}
